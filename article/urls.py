629e6edc7086174c050a25bb8a8b9c830362463e
shishusaiwai <shishusaiwai@vip.qq.com>
2015-10-21 17:24:38 +0800

    lession 6

--- article/urls.py
+++ article/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^lists/(?P<block_id>\d+)', "article.views.article_list", name="article_list"),
+    url(r'^create/(?P<block_id>\d+)/$', "article.views.create_article", name="create_article"),
+    url(r'^detail/(?P<article_id>\d+)/$', "article.views.article_detail", name="article_detail"),
 ]
--- article/views.py
+++ article/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render_to_response
+# coding: utf-8
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
 
 from block.models import Block
 from models import Article
@@ -8,4 +13,33 @@ def article_list(request, block_id):
     block_id = int(block_id)
     block = Block.objects.get(id=block_id)
     articles = Article.objects.filter(block=block).order_by("-last_update_timestamp")
-    return render_to_response("article_list.html", {"articles": articles, "block": block})
+    return render_to_response("article_list.html", {"articles": articles, "b": block},
+                              context_instance=RequestContext(request))
+
+
+def create_article(request, block_id):
+    block_id = int(block_id)
+    block = Block.objects.get(id=block_id)
+    if request.method == "GET":
+        return render_to_response("article_create.html", {"b": block},
+                                  context_instance=RequestContext(request))
+    else:  # request.method == "POST"
+        title = request.POST["title"].strip()
+        content = request.POST["content"].strip()
+        if not title or not content:
+            messages.add_message(request, messages.ERROR, u'标题和内容均不能为空')
+            return render_to_response("article_create.html",
+                                      {"b": block, "title": title, "content": content},
+                                      context_instance=RequestContext(request))
+        owner = User.objects.all()[0]  # TODO:
+        new_article = Article(block=block, owner=owner, title=title, content=content)
+        new_article.save()
+        messages.add_message(request, messages.INFO, u'成功发布文章.')
+        return redirect(reverse("article_list", args=[block.id]))
+
+
+def article_detail(request, article_id):
+    article_id = int(article_id)
+    article = Article.objects.get(id=article_id)
+    return render_to_response("article_detail.html", {"article": article},
+                              context_instance=RequestContext(request))
