ed0c9709b8207a6dce2917c79836f92c9cc69054
shishusaiwai <shishusaiwai@vip.qq.com>
2015-10-24 09:56:28 +0800

    lession 9

--- article/views.py
+++ article/views.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 from django.shortcuts import render_to_response, redirect
@@ -11,9 +12,27 @@ from models import Article
 
 def article_list(request, block_id):
     block_id = int(block_id)
+    page_no = int(request.GET.get("page_no", "1"))
     block = Block.objects.get(id=block_id)
     articles = Article.objects.filter(block=block).order_by("-last_update_timestamp")
-    return render_to_response("article_list.html", {"articles": articles, "b": block},
+
+    p = Paginator(articles, 1)
+    if page_no > p.num_pages:
+        page_no = p.num_pages
+    if page_no <= 0:
+        page_no = 1
+    page_links = [i for i in range(page_no - 5, page_no + 6) if i > 0 and i <= p.num_pages]
+    page = p.page(page_no)
+    previous_link = page_links[0] - 1
+    next_link = page_links[-1] + 1
+    return render_to_response("article_list.html",
+                              {"articles": page.object_list, "b": block,
+                               "has_previous":  previous_link > 0, "has_next": next_link <= p.num_pages,
+                               "previous_link": previous_link,
+                               "next_link": next_link,
+                               "page_cnt": p.num_pages,
+                               "current_no": page_no,
+                               "page_links": page_links},
                               context_instance=RequestContext(request))
 
 
