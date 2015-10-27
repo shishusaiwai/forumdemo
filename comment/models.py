8894f205bbf43a3d1351f01d8cfe8509c9ca7f28
shishusaiwai <shishusaiwai@vip.qq.com>
2015-10-27 15:14:08 +0800

    lession 11, part 1

--- comment/models.py
+++ comment/models.py
@@ -17,6 +17,13 @@ class Comment(models.Model):
     create_timestamp = models.DateTimeField(auto_now_add=True)
     last_update_timestamp = models.DateTimeField(auto_now=True)
 
+    @property
+    def to_comment(self):
+        if not self.to_comment_id:
+            return None
+        else:
+            return Comment.objects.get(id=self.to_comment_id)
+
     def __unicode__(self):
         return self.content[:20]
 
